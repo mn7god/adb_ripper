bfe6d9da9db00b4d542789edb3313a6db1230d92
mn7god <127699732+mn7god@users.noreply.github.com>
2026-03-26 16:18:26 -0300

    Add files via upload

--- adb_ripper.py
+++ adb_ripper.py
@@ -11,7 +11,7 @@ from adb_libs.printit import Color as cl
 from adb_libs.printit import PrintIt as pt
 from adb_libs.adb_session import AdbSession 
 from adb_libs.cmd2_parsers import Parsers as prs
-from adb_libs.maintenance_utils import Maintenance as mt
+from adb_libs.maintent_utils import Maintenance as mt
 
 IP_RE = re.compile(r"^(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)\.(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)\.(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)\.(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)$")
 PORT_RE = re.compile(r"^(0|[1-9]{4,5})$")
@@ -46,30 +46,36 @@ class AdbRipper(cmd2.Cmd):
         elif args.K:
             c, st, sd = mt.exec_cmd(["adb", "disconnect"])
             if c == 0:
-                pt.success("All sessions killed");return
+                pt.success("All sessions killed")
+                return
             
             pt.fail("Failed to kill all adb sessions.")
 
         elif args.L:
             if args.L in devices:
-                SessionManager(args.L).cmdloop();return
+                SessionManager(args.L).cmdloop()
+                return
             
-            pt.fail(f"Device '{args.L}' not found.");return
+            pt.fail(f"Device '{args.L}' not found.")
+            return
 
         elif args.k:
             if args.k in devices:
                 c, st, sd = mt.exec_cmd(["adb", "disconnect", args.k])
                 if c == 0:
-                    pt.success(f"Device '{args.k}' disconnected.");return
+                    pt.success(f"Device '{args.k}' disconnected.")
+                    return
                     
         elif args.c:
             _ip, _port = args.c
             if IP_RE.fullmatch(_ip) and PORT_RE.fullmatch(_port):
                 c, st, sd = mt.exec_cmd(["adb", "connect", f"{_ip}:{_port}"])
                 if c == 0 and not "failed to connect" in sd.lower():
-                    pt.success(f"Connection successfull with device IP '{_ip}'.");return
+                    pt.success(f"Connection successfull with device IP '{_ip}'.")
+                    return
                 
-                pt.fail(f"Cant connect with device IP '{_ip}'.");return
+                pt.fail(f"Can't connect with device IP '{_ip}'.")
+                return
                 
         elif args.C:
             _ip, _ip_port, _pair_port, _pair_code = args.C
@@ -85,13 +91,16 @@ class AdbRipper(cmd2.Cmd):
                     pt.success(f"Paired successfully with device IP '{_ip}'.")
                     c1, st1, sd1 = mt.exec_cmd(["adb", "connect", f"{_ip}:{_ip_port}"])
                     if c1 == 0 and not "failed" in sd.lower():
-                        pt.success(f"Connected successfully with device IP '{_ip}'");return
+                        pt.success(f"Connected successfully with device IP '{_ip}'")
+                        return
                     
-                    pt.fail(f"Cant connect with device IP '{_ip}'.");return
+                    pt.fail(f"Cant connect with device IP '{_ip}'.")
+                    return
                 
-                pt.fail(f"Cant pair with device IP '{_ip}'.");return
+                pt.fail(f"Cant pair with device IP '{_ip}'.")
+                return
 
-        pt.error(f"Unknown instructions please use '{cl.RED}help sessions{cl.RESET}'")
+        pt.incorrect_usage("sessions")
         
     def do_banner(self, arg):
         '''Displays a banner on the screen.'''
@@ -117,28 +126,37 @@ class SessionManager(AdbRipper):
             s = mt.return_sessions()
             if s != {}:
                 table = mt.sessions_formatter(s)
-                print(tabulate(table, headers=["Device","System","Kernel Release","Arch"], tablefmt="simple_grid"))
-            else:
-                pt.fail("No valid sessions online found.")
-
+                print(
+                    tabulate(
+                        table, 
+                        headers=["Device","System","Kernel Release","Arch"], 
+                        tablefmt="simple_grid")
+                    )
+                return
+            
+            pt.fail("No valid sessions online found.")
             return
 
         elif args.K:
             c, st, sd = mt.exec_cmd(["adb", "disconnect"])
             if c == 0:
-                pt.success("All sessions killed");return
+                pt.success("All sessions killed")
+                return
             
-            pt.fail("Failed to kill all adb sessions.")
+            pt.fail("Failed to kill all adb sessions.");
+            return
 
         elif args.k:
             if args.k in devices:
                 c, st, sd = mt.exec_cmd(["adb", "disconnect", args.k])
                 if c == 0:
-                    pt.success(f"Device '{args.k}' disconnected.");return
+                    pt.success(f"Device '{args.k}' disconnected.")
+                    return
             
             pt.fail(f"Device '{args.k}' not found.")
-
-        pt.error(f"Unknown instructions please use '{cl.RED}help sessions{cl.RESET}'")
+            return
+            
+        pt.incorrect_usage("sessions")
         
     def do_sysinf(self, args):
         '''Show device specifications.'''
@@ -148,109 +166,126 @@ class SessionManager(AdbRipper):
     def do_send_key(self, args):
         if args.key:
             if args.key < 287 and args.key >= 0:
-                self.session.send_key(str(args.key));return
+                self.session.send_key(str(args.key))
+                return
 
-        pt.error(f"Incorrect usage, use {cl.RED}help send_key{cl.RESET}.")
+        pt.incorrect_usage("send_key")
 	
     @cmd2.with_argparser(prs.send_keys_parser)
     def do_send_keys(self, args):
         if args.keys:
-            self.session.multikey(args.keys);return
+            self.session.multikey(args.keys)
+            return
 
-        pt.error(f"Incorrect usage, use {cl.RED}help send_keys{cl.RESET}.")
+        pt.incorrect_usage("send_keys")
     
     @cmd2.with_argparser(prs.send_text_parser)
     def do_send_text(self, args):
         if args.text:
             text = " ".join(args.text)
-            self.session.send_text(text);return
+            self.session.send_text(text)
+            return
 
-        pt.error(f"Incorrect usage, use {cl.RED}help send_text{cl.RESET}.")
+        pt.incorrect_usage("send_text")
         
     @cmd2.with_argparser(prs.search_parser)
     def do_search(self, args):
         
         if args.term:
-            self.session.search(term=args.term);return
+            self.session.search(term=args.term)
+            return
 
-        pt.error(f"Incorrect usage, use {cl.RED}help search{cl.RESET}.")
+        pt.incorrect_usage("search")
     
     @cmd2.with_argparser(prs.ripper_parser)
     def do_ripper(self, args):
         
         if args.l:
-            self.session.ripper("list"); return
+            self.session.ripper("list")
+            return
             
         elif args.r:
             _payload, _delay = args.r
             if mt.check_int(_delay):
-                self.session.ripper(mode="run", payload=_payload, delay=int(_delay));return
+                self.session.ripper(mode="run", payload=_payload, delay=int(_delay))
+                return
             
-            self.session.ripper(mode="run", payload=_payload);return
+            self.session.ripper(mode="run", payload=_payload)
+            return
 
-        pt.error(f"Incorrect usage, use {cl.RED}help ripper{cl.RESET}.")
+        pt.incorrect_usage("ripper")
         
     @cmd2.with_argparser(prs.clear_pkg_parser)
     def do_clear_pkg(self, args):
         if args.pkg:
-            self.session.clear_package(args.pkg);return 
+            self.session.clear_package(args.pkg)
+            return 
             
-        pt.error(f"Incorrect usage, use {cl.RED}help clear_pkg{cl.RESET}.")
+        pt.incorrect_usage("sessions")
         
     @cmd2.with_argparser(prs.uninstall_parser)
     def do_uninstall(self, args):
         if args.pkg:
-            self.session.uninstall(args.pkg);return 
+            self.session.uninstall(args.pkg)
+            return 
             
-        pt.error(f"Incorrect usage, use {cl.RED}help uninstall{cl.RESET}.")
+        pt.incorrect_usage("uninstall")
         
     @cmd2.with_argparser(prs.install_parser)
     def do_install(self, args):
         if args.apk:
-            self.session.install(args.apk);return
+            self.session.install(args.apk)
+            return
         
-        pt.error(f"Incorrect usage, use {cl.RED}help install{cl.RESET}.")
+        pt.incorrect_usage("install")
         
     @cmd2.with_argparser(prs.list_pkgs_parser)
     def do_list_pkgs(self, args):
         
         if not args.term:
-            self.session.list_packages();return
+            self.session.list_packages()
+            return
+        
         elif args.term:
-            self.session.list_packages(term=args.term);return
+            self.session.list_packages(term=args.term)
+            return
             
-        pt.error(f"Incorrect usage, use {cl.RED}help list_pkgs{cl.RESET}.")
+        pt.incorrect_usage("list_pkgs")
     
     @cmd2.with_argparser(prs.get_prop_parser)
     def do_get_prop(self, args):
 
         if not args.term:
-            self.session.getprop();return
+            self.session.getprop()
+            return
+            
         elif args.term:
-            self.session.getprop(term=args.term);return
+            self.session.getprop(term=args.term)
+            return
         
-        pt.error(f"Incorrect usage, use {cl.RED}help list_pkgs{cl.RESET}.")
+        pt.incorrect_usage("get_prop")
         
     @cmd2.with_argparser(prs.start_app_parser)
     def do_start_app(self, args):
         if args.pkg:
-            self.session.start_app(args.pkg);return
+            self.session.start_app(args.pkg)
+            return
             
-        pt.error(f"Incorrect usage, use {cl.RED}help start_app{cl.RESET}.")
+        pt.incorrect_usage("start_app")
         
     @cmd2.with_argparser(prs.send_parser)
     def do_send(self, args):
         if args.local_path and args.dest_path:
             self.session.send(args.local_path, args.dest_path);return
             
-        pt.error(f"Incorrect usage, use {cl.RED}help send{cl.RESET}.")
+        pt.incorrect_usage("send")
         
     @cmd2.with_argparser(prs.dump_parser)
     def do_dump(self, args):
         if args.remote_path and args.local_path:
             self.session.dump(args.remote_path, args.local_path);return
 
-        pt.error(f"Incorrect usage, use {cl.RED}help dump{cl.RESET}.")
+        pt.incorrect_usage("dump")
         
     def do_raw_shell(self, args):
         '''Starts a raw shell in device.
@@ -277,28 +312,33 @@ usage: shell'''
                 cleaned.append(f".{item}")
         
             if not cleaned:
-                pt.error("No valid extensions provided.");return
+                pt.error("No valid extensions provided.")
+                return
         
             self.session.dump_sd(tuple(cleaned))
             return
 
-        pt.error(f"Incorrect usage, use {cl.RED}help dump_sd{cl.RESET}.")
+        pt.incorrect_usage("dump_sd")
         
     @cmd2.with_argparser(prs.input_spam_parser)
     def do_input_spam(self, args):
         if args.s:
-            self.session.input_spam(mode="swipe-random");return
+            self.session.input_spam(mode="swipe-random")
+            return
         
         elif args.t:
-            self.session.input_spam(mode="tap-random");return
+            self.session.input_spam(mode="tap-random")
+            return
         
         elif args.k:
-            self.session.input_spam(mode="keyevent-random");return
+            self.session.input_spam(mode="keyevent-random")
+            return
         
         elif args.p:
-            self.session.input_spam(mode="press-spam");return
+            self.session.input_spam(mode="press-spam")
+            return
             
-        pt.error(f"Incorrect usage, use {cl.RED}help input_spam{cl.RESET}.")
+        pt.incorrect_usage("input_spam")
         
     def do_live(self, args):
         self.session.live()
@@ -313,14 +353,14 @@ usage: dump_wpp'''
         if args.out:
             self.session.screenrecord(args.out);return
             
-        pt.error(f"Incorrect usage, use {cl.RED}help screenrecord{cl.RESET}.")
+        pt.incorrect_usage("screenrecord")
         
     @cmd2.with_argparser(prs.screencap_parser)
     def do_screencap(self, args):
         if args.out:
             self.session.screencap(args.out);return
             
-        pt.error(f"Incorrect usage, use {cl.RED}help screencap{cl.RESET}.")
+        pt.incorrect_usage("screencap")
         
 if __name__ == "__main__":
     if mt.check_adb():
